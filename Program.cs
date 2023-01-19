f3c89e7530846b67d64680e817d78a6db15a55df
T N Thanoof Naina <thanoof.tn@experionglobal.local>
2023-01-19 09:25:15 +0530

    CC9

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿var ages = new Dictionary<int, string>();
+int key = 0;
+string value = null;
+
+for(int i = 0; i < 1; i++)
+{
+    Console.WriteLine($"Enter the date of birth of person {i+1}");
+    DateTime dob = new DateTime();
+    DateTime.TryParse(Console.ReadLine(), out dob);
+    DateTime now = DateTime.Now;
+ 
+    key = now.Year - dob.Year;
+    if (key < 10)
+    {
+        value = "kid";
+    }
+    else if (key >= 10 && key < 30)
+    {
+        value = "Youth";
+    }
+    else if (key >= 30 && key < 60)
+    {
+        value = "Adult";
+    }
+    else if (key >= 60)
+    {
+        value = "Older";
+    }
+    ages[key] = value;
+}
+
+foreach (var i in ages)
+{
+    Console.WriteLine($"The person has an age {i.Key} and is categorised as {i.Value}.");
+}
